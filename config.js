189363bd60b8e967c18c1c7c80603cf9e756df90
Cole Ellison <cole.ellison@gmail.com>
2016-06-11 10:00:01 -0400

    Deleting files that should be ignored

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = { chID: 'C1CNRUASV', apiTok: 'sksksk' }
-// C1FLCK3KL - testbots // C1CNRUASV - help
-// old api - 'xoxb-49652013520-6hbAGqWtrI5HMvvwPbU5vcKS'
-// ethan, moose, lando, josh, abhi, lane, darwish
-// var TAs = ['U19N2AGJW','U14F38LEP','U14PMSWHW','U19NBBH44','U14F0BAUT','U1ALJ2H89','U14G3MRSL']
